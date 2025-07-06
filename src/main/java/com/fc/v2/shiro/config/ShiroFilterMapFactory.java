3c265ce979615ae7ee08f617fb5ff9dfb9110f67
g-nogarawbwoc <kangzi1465654@126.com>
2025-07-06 22:51:04 +0800

    防止注入

--- src/main/java/com/fc/v2/shiro/config/ShiroFilterMapFactory.java
+++ src/main/java/com/fc/v2/shiro/config/ShiroFilterMapFactory.java
@@ -60,7 +60,7 @@ public class ShiroFilterMapFactory {
 		filterChainDefinitionMap.put("/ApiController/**", "anon");
 
 		filterChainDefinitionMap.put("/oss/**", "anon");
-		filterChainDefinitionMap.put("/druid/**", "anon");
+		//filterChainDefinitionMap.put("/druid/**", "anon");
 
 		// 对所有页面进行认证
 		filterChainDefinitionMap.put("/**", "user");
